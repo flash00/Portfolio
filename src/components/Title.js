d63476385301fd73147036f73fe644f1372690f1
flash00 <dexlorraine@gmail.com>
2018-07-08 15:55:00 -0700

    Delete old files.

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,7 +7,7 @@ class Title extends Component {
     return (
       <div className="">
         <div className="container">
-          <h1 className="title">Hello, my name is Lorraine. I am a Developer living in the San Francisco Bay Area.</h1>
+          <h1 className="title">Hello, my name is Lorraine. I am a Software Engineer and Developer living in the San Francisco Bay Area.</h1>
         </div>
         <div className="container-flex">
           <img className="title-pic" src={require('./hand sun water.jpg')} alt="Photo by Lukas on Pexels"/>
